69ded47695c202ca7553227869f3fe169361cfc8
maojiawei <maojiawei2012180@126.com>
2021-04-18 21:24:25 +0800

    feature: sentinel start

---
+++ ancientpower-sentinel/src/main/java/io/jovi/ancientpower/sentinel/config/SentinelConfig.java
@@ -0,0 +1,21 @@
+package io.jovi.ancientpower.sentinel.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * <p>
+ * Title:
+ * </p>
+ * <p>
+ * Description:
+ * </p>
+ * <p>
+ * Copyright: Copyright (c) 2020
+ * </p>
+ *
+ * @author MaoJiaWei
+ * @version 1.0
+ */
+@Configuration
+public class SentinelConfig {
+}
